5662a59608da9357e8646a997609aaf469d8920b
Kevin Mark <kmark1625@gmail.com>
2016-10-22 17:08:48 -0700

    Update wiring of items

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -1,14 +1,36 @@
 'use strict';
 
-angular.module('myApp.view2', ['ngRoute'])
+angular.module('myApp.view2', ['ngRoute', 'services'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view2', {
+  $routeProvider.when('/view2/:itemId', {
     templateUrl: 'view2/view2.html',
-    controller: 'View2Ctrl'
+    controller: 'View2Ctrl as vm'
   });
 }])
 
-.controller('View2Ctrl', [function() {
+.controller('View2Ctrl', ['$routeParams', 'ItemService', function($routeParams, ItemService) {
+    var vm = this;
 
+    // Methods
+    vm.init = init;
+    vm.getItem = getItem;
+
+    // Properties
+    vm.itemId;
+    vm.item = {};
+    vm.itemService = ItemService;
+
+    // Init
+    vm.init();
+    
+    // Method Definitions
+    function init() {
+        vm.itemId = $routeParams.itemId;
+        vm.item = vm.getItem(vm.itemId);
+    }
+
+    function getItem(itemId) {
+        return vm.itemService.getItem(itemId);
+    }
 }]);
