506fc9382634579e4aca8e14e532e80aef4bf0d5
Kevin Mark <kmark1625@gmail.com>
2016-10-23 07:27:12 -0700

    Style updates

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.view2', ['ngRoute', 'services'])
+angular.module('myApp.view2', ['ngRoute', 'services', 'ui.bootstrap'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view2/:itemId', {
@@ -9,10 +9,11 @@ angular.module('myApp.view2', ['ngRoute', 'services'])
   });
 }])
 
-.controller('View2Ctrl', ['$routeParams', 'ItemService', function($routeParams, ItemService) {
+.controller('View2Ctrl', ['$routeParams', 'ItemService', '$uibModal', function($routeParams, ItemService, $uibModal) {
     var vm = this;
 
     // Methods
+    vm.bid = bid;
     vm.init = init;
     vm.getItem = getItem;
 
@@ -25,6 +26,20 @@ angular.module('myApp.view2', ['ngRoute', 'services'])
     vm.init();
     
     // Method Definitions
+    function bid() {
+        vm.bidModal = $uibModal.open({
+            templateUrl: '',
+            controller: ''
+        })
+    }
+
+    function guess() {
+        vm.guessModal = $uibModal.open({
+            templateUrl: '',
+            controller: ''
+        })
+    }
+
     function init() {
         vm.itemId = $routeParams.itemId;
         vm.item = vm.getItem(vm.itemId);
