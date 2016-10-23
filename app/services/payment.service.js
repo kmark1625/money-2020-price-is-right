98cde75e0141a3428eddbb0d96dc600b056b0376
Kevin Mark <kmark1625@gmail.com>
2016-10-23 10:53:30 -0700

    Updates

--- app/services/payment.service.js
+++ app/services/payment.service.js
@@ -10,7 +10,25 @@
 
         // Methods
         paymentService.buyItem = buyItem;
+        paymentService.payUser = payUser;
 
+        function payUser(amount) {
+            $http({
+                method: 'GET',
+                url: 'http://pir2020.azurewebsites.net/transfer/' + amount,
+                //data: amount,
+                //headers: {'Content-Type' : 'application/json'}
+            }).then(function successCallback(response) {
+                // this callback will be called asynchronously
+                // when the response is available
+                console.log(JSON.stringify(response));
+
+            }, function errorCallback(response) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+                console.log(JSON.stringify(response));
+            });
+        }
         function buyItem(amount) {
             $http({
                 method: 'POST',
